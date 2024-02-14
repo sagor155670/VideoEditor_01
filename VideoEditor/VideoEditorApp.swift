01ce4131598c1ea6a4fbda67f9b97e90ad4c1bbd
sagor155670 <sagor@braincraftapps.com>
2024-02-14 16:13:30 +0600

    Initial Commit

---
+++ VideoEditor/VideoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorApp.swift
+//  VideoEditor
+//
+//  Created by Saiful Islam Sagor on 14/2/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
